c936006a479f26646011530703d08fac01435a56
andrepapaz <andre.papazoglu@gmail.com>
2017-03-26 22:01:35 -0300

    Tratamento de erro no Service

--- src/domain/foto/FotoService.js
+++ src/domain/foto/FotoService.js
@@ -15,11 +15,19 @@ export default class FotoService {
     lista() {
         return this._resource
             .query()
-            .then(res => res.json());
+            .then(res => res.json(), err =>{
+                console.log(err);
+                throw new Error('Não foi possível obter as fotos. Tente mais tarde.');
+            });
     }
 
     apaga(id) {
-        return this._resource.delete({ id });
+        return this._resource
+            .delete({ id })
+            .then(null, err => {
+                console.log(err);
+                throw new Error('Não foi possível remover a foto.');
+            });
     }
 
     busca(id) {
