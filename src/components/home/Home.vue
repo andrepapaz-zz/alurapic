ef6d774d506521396b0418c27df281d93465726d
andrepapaz <andre.papazoglu@gmail.com>
2017-03-29 22:08:13 -0300

    Criando pasta static para imagens

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
+
+    <img src="/static/teste.png" alt="imagem do lobo">
     <h1 class="centralizado">{{ titulo }}</h1>
     
     <p v-show="mensagem" class="centralizado">{{ mensagem }}</p>
